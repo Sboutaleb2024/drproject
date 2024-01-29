8cc6ab15f5bc8f5915e9ea8dff0ee2188d721991
Simon Navarro <simonairamnavarro@gmail.com>
2024-01-29 15:52:34 -0600

    First init

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavaigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
